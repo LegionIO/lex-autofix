5af06327055f8b4e5f1c5473d3f6c821e2d5342a
Esity <matthewdiverson@gmail.com>
2026-03-21 23:38:56 -0500

    add log consumer subscription actor

---
+++ lib/legion/extensions/autofix/actors/log_consumer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Legion
+  module Extensions
+    module Autofix
+      module Actors
+        class LogConsumer < Legion::Extensions::Actors::Subscription
+          def runner_function = 'handle_log_event'
+          def check_subtask? = false
+          def generate_task? = false
+
+          def enabled?
+            !!(defined?(Legion::LLM) && Legion::LLM.respond_to?(:started?) && Legion::LLM.started?)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/actors/log_consumer_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+unless defined?(Legion::Extensions::Actors::Subscription)
+  module Legion
+    module Extensions
+      module Actors
+        class Subscription; end # rubocop:disable Lint/EmptyClass
+      end
+    end
+  end
+end
+
+require 'legion/extensions/autofix/actors/log_consumer'
+
+RSpec.describe Legion::Extensions::Autofix::Actors::LogConsumer do
+  subject(:actor) { described_class.allocate }
+
+  describe '#runner_function' do
+    it 'returns handle_log_event' do
+      expect(actor.runner_function).to eq('handle_log_event')
+    end
+  end
+
+  describe '#check_subtask?' do
+    it 'returns false' do
+      expect(actor.check_subtask?).to be(false)
+    end
+  end
+
+  describe '#generate_task?' do
+    it 'returns false' do
+      expect(actor.generate_task?).to be(false)
+    end
+  end
+
+  describe '#enabled?' do
+    context 'when Legion::LLM is defined and started? returns true' do
+      before do
+        stub_const('Legion::LLM', Module.new do
+          def self.started? = true
+        end)
+      end
+
+      it 'returns true' do
+        expect(actor.enabled?).to be(true)
+      end
+    end
+
+    context 'when Legion::LLM is defined but started? returns false' do
+      before do
+        stub_const('Legion::LLM', Module.new do
+          def self.started? = false
+        end)
+      end
+
+      it 'returns false' do
+        expect(actor.enabled?).to be(false)
+      end
+    end
+
+    context 'when Legion::LLM is not defined' do
+      before { hide_const('Legion::LLM') }
+
+      it 'returns false' do
+        expect(actor.enabled?).to be(false)
+      end
+    end
+  end
+end
