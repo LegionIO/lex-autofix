9ee652364ba1bdeacec51f5fc724c2f737871f01
Esity <matthewdiverson@gmail.com>
2026-03-21 23:45:33 -0500

    wire entry point and verify full suite

--- lib/legion/extensions/autofix.rb
+++ lib/legion/extensions/autofix.rb
@@ -13,3 +13,19 @@ module Legion
     end
   end
 end
+
+# Require components when framework is available
+if defined?(Legion::Extensions::Core)
+  require_relative 'autofix/helpers/batch_buffer'
+  require_relative 'autofix/helpers/prompts'
+  require_relative 'autofix/helpers/temp_checkout'
+  require_relative 'autofix/helpers/client'
+  require_relative 'autofix/runners/triage'
+  require_relative 'autofix/runners/diagnose'
+  require_relative 'autofix/runners/fix'
+  require_relative 'autofix/runners/ship'
+  require_relative 'autofix/runners/pipeline'
+  require_relative 'autofix/client'
+  require_relative 'autofix/transport' if defined?(Legion::Extensions::Transport)
+  require_relative 'autofix/actors/log_consumer' if defined?(Legion::Extensions::Actors::Subscription)
+end
